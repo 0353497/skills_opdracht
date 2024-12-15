065ff7b7c4b4adf82f09863ea072a424c4a4e292
Niek Geerligs <144774725+0353497@users.noreply.github.com>
2024-12-15 14:53:42 +0100

    added big button

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -32,11 +34,62 @@ class HomePage extends StatelessWidget {
               description:
                   'De Montelbaanstoren is een toren in Amsterdam, gelegen aan de Oudeschans bij de Lastage, een voormalige haven in het oosten van de oude binnenstad.',
             ),
-            
+            SizedBox(
+              height: 20,
+            ),
+            OwnCard(
+                url:
+                    'https://images.unsplash.com/photo-1731370963500-b836d108f7c9?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+                title: 'Rijksmuseum',
+                subtitle: 'Een bijzondere ervaring',
+                description:
+                    'Het Rijksmuseum is een Nederlands rijksmuseum voor kunst en geschiedenis in Amsterdam.'),
+            SizedBox(
+              height: 20,
+            ),
+            OwnCard(
+                url:
+                    'https://images.unsplash.com/photo-1731370963500-b836d108f7c9?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+                title: 'Anne Frank Huis',
+                subtitle: 'Een indrukwekkende ervaring',
+                description: 'Het Anne Frank Huis is een museum in Amsterdam.'),
+            SizedBox(
+              height: 20,
+            ),
+            //add 3 big buttons
+            BigButton(text: 'Kaart'),
+            SizedBox(
+              height: 20,
+            ),
+            BigButton(text: 'Hulp'),
+            SizedBox(
+              height: 20,
+            ),
+            BigButton(text: 'Attracties'),
           ],
         ),
       ),
     ));
+  }
+}
+
+class BigButton extends StatelessWidget {
+  const BigButton({
+    super.key,
+    required String text,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return FilledButton(
+      onPressed: () {},
+      child: const Text('Kaart'),
+      style: ButtonStyle(
+          elevation: WidgetStatePropertyAll(10),
+          padding: WidgetStatePropertyAll(
+              EdgeInsets.symmetric(vertical: 30, horizontal: 60)),
+          backgroundColor: WidgetStatePropertyAll(Colors.blue)),
+    );
   }
 }
 
